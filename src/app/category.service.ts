76a459cb0135e1519d1d20fff1e3bd2a466660fa
syntax-error35 <46873800+syntax-error35@users.noreply.github.com>
2020-08-10 14:32:06 +0600

    Update category.service.ts

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+  aflCategories: AngularFireList<any>;
+  constructor(private db: AngularFireDatabase) {}
 
-  constructor(private db: AngularFireDatabase) { }
-
-  getCategories(){
-    return this.db.list('/categories');
-  }
+   getCategories(){
+     this.aflCategories = this.db.list('/categories', category => category.orderByChild('name'));
+     return this.aflCategories
+     .snapshotChanges()
+     .pipe(map(changes => changes
+     .map(c => ({ key: c.payload.key, ...c.payload.val() }))));
+   }
 }
