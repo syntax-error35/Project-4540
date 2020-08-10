181dc600030cd5d317149482541973810d8e88a2
syntax-error35 <46873800+syntax-error35@users.noreply.github.com>
2020-08-10 14:36:54 +0600

    Update product-form.component.ts

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/category.service';
+import { AngularFireList, AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.css']
 })
-export class ProductFormComponent implements OnInit {
-  categories$;
+  export class ProductFormComponent {
+  categories$: Observable<any[]>;
 
-  constructor(categoryService: CategoryService) { 
+  constructor(
+    categoryService: CategoryService,
+    private db: AngularFireDatabase
+  ) {
     this.categories$ = categoryService.getCategories();
   }
 
